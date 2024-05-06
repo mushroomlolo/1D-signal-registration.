946d873d2a1b2b13ad4dc36d3c605dda2c25b282
mushroomlolo <32677181+mushroomlolo@users.noreply.github.com>
2024-05-06 22:32:49 +0800

    Update Operation.py

--- Operation.py
+++ Operation.py
@@ -186,3 +186,24 @@ def find_weight_max(array1, array2):
         return array1_len - max_index_1
     else:
         return max_index_2 - array2_len
+
+
+if __name__ == "__main__":
+    signal_1 = np.genfromtxt('signal1.csv', delimiter=',', encoding='utf-8-sig')
+    signal_2 = np.genfromtxt('signal2.csv', delimiter=',', encoding='utf-8-sig')
+
+    #
+    s, result = signal_align(signal_1, signal_2, 10)
+    print('Shift value to align is', s)
+
+    plt.subplot(2, 1, 1)
+    plt.plot(signal_1, label='singal_1')
+    plt.plot(signal_2, label='singal_2')
+    plt.legend(loc='best')
+    plt.ylim(bottom=0)
+
+    plt.subplot(2, 1, 2)
+    plt.plot(result, label='registration')
+    plt.legend(loc='best')
+    plt.ylim(bottom=0)
+    plt.show()
